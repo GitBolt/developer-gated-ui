70fa9782eeb1e0fe88e0ef7bb45b37806e1edbbe
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-05-27 08:03:48 +0530

    feat: added verify

---
+++ packages/backend/src/controllers/verify.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import * as anchor from '@coral-xyz/anchor';
+import { prisma } from '../utils';
+import { validatePubkey } from '../utils/validatePubkey';
+export const verifyUser = async (req: Request, res: Response) => {
+  const { signature, github, publicKey } = req.body;
+  if (!validatePubkey(publicKey)) {
+    return res.status(400).json({ message: 'Invalid public key' });
+  }
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        github: github,
+      },
+    });
+    if (!user) {
+      return res
+        .status(204)
+        .json({ message: 'Github username do not exist in the list' });
+    }
+    if (user?.signature && user.wallet) {
+      return res.status(200).json({ message: 'User already exists' });
+    }
+
+    await prisma.user.update({
+      where: {
+        github: github,
+      },
+      data: {
+        signature: signature,
+        wallet: publicKey,
+      },
+    });
+
+    return res.status(200).json({ message: 'User verified successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ packages/backend/src/utils/validatePubkey.ts
@@ -0,0 +1,13 @@
+import * as anchor from '@coral-xyz/anchor';
+
+export const validatePubkey = (pubkey: string): boolean => {
+  try {
+    const key = new anchor.web3.PublicKey(pubkey);
+
+    return true;
+  } catch (error) {
+    console.log(error);
+
+    return false;
+  }
+};
